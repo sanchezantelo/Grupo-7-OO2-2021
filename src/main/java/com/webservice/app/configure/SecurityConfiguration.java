01664ef9e22b7f73a0e2a19c2b4fdb59820f04a1
Sofía Sánchez Antelo <sofiasa@live.com.ar>
2021-04-28 15:27:44 -0300

    Se agrega package controller y configure y login html y error

---
+++ src/main/java/com/webservice/app/configure/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.webservice.app.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+	    http
+	        .formLogin()
+	        .loginPage("/login.html")
+	        .failureUrl("/login-error.html")
+	      .and()
+	        .logout()
+	        .logoutSuccessUrl("/index.html");
+	}
+}
--- src/main/java/com/webservice/app/controller/GreetingController.java
+++ src/main/java/com/webservice/app/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.webservice.app;
+package com.webservice.app.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,5 +30,19 @@ public class GreetingController {
         logger.info("Message" + name);
         model.addAttribute("name", name);
         return model.getAttribute("name").toString();
+    }
+    
+
+    // Login form
+    @RequestMapping("/login.html")
+    public String login() {
+      return "login.html";
+    }
+
+    // Login form with error
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+      model.addAttribute("loginError", true);
+      return "login.html";
     }
 }
