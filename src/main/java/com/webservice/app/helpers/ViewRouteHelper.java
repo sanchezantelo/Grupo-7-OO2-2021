cbb8a5ee446936c0739450b522bfd2d87762b304
PATLP <pat_lp@outlook.com>
2021-05-14 11:54:49 -0300

    Revert "Merge pull request #7 from sanchezantelo/dev"
    This reverts commit 37b47b872abdedcb7df85a452d9c0286f00e9de7, reversing
    changes made to a81db4f746ebb945c9bc627ad5ce7c62d02aff6a.

--- src/main/java/com/webservice/app/helpers/ViewRouteHelper.java
+++ src/main/java/com/webservice/app/helpers/ViewRouteHelper.java
@@ -3,21 +3,14 @@ package com.webservice.app.helpers;
 public class ViewRouteHelper {
  
 	/**** Views ****/
-
-	// USUARIO-LOGIN
+	
+	//USER
 	public final static String USER_LOGIN = "user/login";
 	public final static String USER_LOGOUT = "user/logout";
-
-	// USUARIO
-	public final static String USUARIO_INDEX = "usuario/index";
-	public final static String USUARIO_NEW = "usuario/new";
-	public final static String USUARIO_UPDATE = "usuario/update";
-	public final static String USUARIO_PARTIAL_VIEW = "usuario/partialUsuarioView";
 	
-	/**** Redirects ****/
-	public final static String ROUTE = "/index";
-	public final static String USUARIO_ROOT = "/usuario";
+	
 
+	
 
 
 }
