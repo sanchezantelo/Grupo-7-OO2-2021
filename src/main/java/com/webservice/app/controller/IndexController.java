baa71cda824d9999997d3baed5c5551fa4ecace1
PATLP <pat_lp@outlook.com>
2021-05-23 19:29:37 -0300

    Update IndexController.java

--- src/main/java/com/webservice/app/controller/IndexController.java
+++ src/main/java/com/webservice/app/controller/IndexController.java
@@ -1,5 +1,21 @@
 package com.webservice.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.webservice.app.helpers.ViewRouteHelper;
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
+
+	
+	@GetMapping("")
+    public ModelAndView index(){
+        ModelAndView mAV = new ModelAndView(ViewRouteHelper.INDEX);
+        return mAV;
+    }
 
 }
