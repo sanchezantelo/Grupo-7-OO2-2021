ab191ee0ce47e0b2ebbe51edb6fd0a230af83379
Sofía Sánchez Antelo <sofiasa@live.com.ar>
2021-06-07 18:41:16 -0300

    Se agrega buscador por dni, genera codigo qr funcionando

--- src/main/java/com/webservice/app/controller/QRCodeController.java
+++ src/main/java/com/webservice/app/controller/QRCodeController.java
@@ -1,33 +1,78 @@
 package com.webservice.app.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.webservice.app.models.PermisoDiarioModel;
+import com.webservice.app.models.PermisoPeriodoModel;
+import com.webservice.app.models.PersonaModel;
+import com.webservice.app.services.IPermisoService;
 import com.webservice.app.services.IQRCodeGeneratorService;
 
-@RestController
-
+@Controller
+@RequestMapping("/buscarPermiso")
 public class QRCodeController {
 
 	@Autowired
 	@Qualifier("qrGeneratorService")
-	private IQRCodeGeneratorService QRCodeGeneratorService;
+	private IQRCodeGeneratorService qrGeneratorService;
+	
+	@Autowired
+	@Qualifier("permisoService")
+	private IPermisoService permisoService;
 
 	private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
 
-	@GetMapping(value = "/genrateAndDownloadQRCode/{codeText}/{width}/{height}")
-	public void download(@PathVariable("codeText") String codeText, @PathVariable("width") Integer width,
-			@PathVariable("height") Integer height) throws Exception {
-		QRCodeGeneratorService.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
-	}
+	private static final String URL_PERMISO = "http://localhost:8080/buscarPermiso?idPermiso=";
 
-	@GetMapping(value = "/genrateQRCode/{codeText}/{width}/{height}")
-	public ResponseEntity<byte[]> generateQRCode(@PathVariable("codeText") String codeText,
-			@PathVariable("width") Integer width, @PathVariable("height") Integer height) throws Exception {
-		return ResponseEntity.status(HttpStatus.OK).body(QRCodeGeneratorService.getQRCodeImage(codeText, width, height));
-	}
+	Logger logger = LoggerFactory.getLogger(QRCodeController.class);
+
+	@GetMapping("")
+	public String buscador(Model model) {
+		model.addAttribute("personaModel", new PersonaModel());
+		model.addAttribute("permisoDiarioModel", new PermisoDiarioModel());
+		model.addAttribute("permisoPeriodoModel", new PermisoPeriodoModel());
+
+
+		return "qrbuscador";
+    }
+	
+	// TRAER PERMISO POR QR
+
+		@PostMapping("/traerPermisoQR")
+		public void traerPermiso(@ModelAttribute("personaModel") PersonaModel personaModel, Model model,
+				RedirectAttributes redirectAttrs,HttpServletResponse response) {
+			logger.info("/traerPermisoDiario" + personaModel);
+			response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+			
+			try {
+				InputStream targetStream = new ByteArrayInputStream(qrGeneratorService.getQRCodeImage(URL_PERMISO+permisoService.findByPersonaDiario(personaModel.getDni()).getIdPermiso(), 400, 400));
+				 IOUtils.copy(targetStream, response.getOutputStream());
+				
+			} catch (Exception e) {
+				redirectAttrs.addFlashAttribute("error", e.getMessage()).addFlashAttribute("clase", "alert alert-danger")
+				;
+
+			}
+		}
+
 }
